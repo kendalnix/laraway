338e5fa41a2844ead7a8b9d7be34168fa201112a
juancparama <juancpc19@educastur.es>
2022-10-25 01:44:04 +0200

    db-migrations

--- database/migrations/2000_08_29_220718_create_locations_table.php
+++ database/migrations/2000_08_29_220718_create_locations_table.php
@@ -13,17 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-
-            // $table->unsignedBigInteger('location_id')->nullable();
-            // $table->foreign('location_id')->references('id')->on('locations');
-            
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -35,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('locations');
     }
 };
